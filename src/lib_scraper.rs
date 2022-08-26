7338f8cc4c35a3cce5219d18a9483a2e19928829
Luis De Lima <luis.delima@kavak.com>
2022-08-26 14:22:08 +0200

    feat: scraping crossfit movement list from wodwell.com

--- src/lib_scraper.rs
+++ src/lib_scraper.rs
@@ -2,14 +2,16 @@ use reqwest;
 use scraper::{Html, Selector};
 use std::process::exit;
 
-const BASE_URL: &str = "https://garagegymbuilder.com/crossfit-hero-wod-masterlist/";
+const BASE_URL_HEROES: &str = "https://garagegymbuilder.com/crossfit-hero-wod-masterlist/";
+const BASE_URL_MOVEMENTS: &str = "https://wodwell.com/browse/?ref=footer#movement";
 
-pub(crate) const WOD_NAMES: &str = "h3#tab-con-3";
-pub(crate) const WOD_TYPE: &str = "p span.bold_text";
-pub(crate) const WOD_DETAILS: &str = "ul.tve_ul.tve_ul1.tve_green";
+pub(crate) const SELECTOR_WOD_NAMES: &str = "h3#tab-con-3";
+pub(crate) const SELECTOR_WOD_TYPE: &str = "p span.bold_text";
+pub(crate) const SELECTOR_WOD_DETAILS: &str = "ul.tve_ul.tve_ul1.tve_green";
+pub(crate) const SELECTOR_MOVEMENTS: &str = "a.wod-collections-list__item";
 
-pub fn get_web_content() -> String {
-    let response = reqwest::blocking::get(BASE_URL).unwrap();
+pub fn get_web_content(url: &str) -> String {
+    let response = reqwest::blocking::get(url).unwrap();
     if !response.status().is_success() {
         println!("Request error: {}", response.status());
         exit(0);
@@ -17,8 +19,13 @@ pub fn get_web_content() -> String {
     return response.text().unwrap();
 }
 
-pub fn get_html_content() -> Html {
-    let html_content = get_web_content();
+pub fn get_html_wod_content() -> Html {
+    let html_content = get_web_content(BASE_URL_HEROES);
+    return Html::parse_document(&html_content);
+}
+
+pub fn get_html_movements_content() -> Html {
+    let html_content = get_web_content(BASE_URL_MOVEMENTS);
     return Html::parse_document(&html_content);
 }
 
--- src/lib_text_processor.rs
+++ src/lib_text_processor.rs
@@ -1,3 +1,5 @@
+use regex::Regex;
+
 pub fn remove_pref(text: String, pref: &str) -> String {
     let sanitized = text.split(pref).collect::<Vec<&str>>()[1].trim();
     return sanitized.to_string();
@@ -6,6 +8,12 @@ pub fn remove_pref(text: String, pref: &str) -> String {
 pub fn get_words_from_sentence(sentence: &str) -> Vec<&str> {
     return sentence.split(" ").collect::<Vec<&str>>();
 }
+
+pub fn remove_issues_from_text(text: String) -> String {
+    let regex = Regex::new(r"\([^)]*\)").unwrap(); // remove parenthesis
+    return regex.replace_all(&text, "").to_string();
+}
+
 
 pub fn get_word_position(text: &String, word: &str) -> i32 {
     let words = get_words_from_sentence(text);
