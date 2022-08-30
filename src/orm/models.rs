87fc709d47d66a79ed38364c612e2d9c0281caba
Luis De Lima <luis.delima@kavak.com>
2022-08-30 23:45:07 +0200

    config: add orm diesel for database operations handler

---
+++ src/orm/models.rs
@@ -0,0 +1,10 @@
+use diesel::prelude::*;
+
+#[derive(Queryable)]
+#[table_name = "tbl_measure"]
+pub struct Measure {
+    pub id: i32,
+    pub name: String,
+    pub created_at: String,
+    pub updated_at: String,
+}
---
+++ src/orm/schema.rs
@@ -0,0 +1,8 @@
+table! {
+    casbin_rule (id) {
+        id -> Integer,
+        name -> Varchar,
+        created_at -> Varchar,
+        updated_at -> Varchar,
+    }
+}
